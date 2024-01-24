c8a1ea783ddc49004718887ea0f67550909af749
Sammed Sagare <76863715+sammedsagare@users.noreply.github.com>
2024-01-24 23:25:42 +0530

    basic structure of mountains website

---
+++ mntn_mountains/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import { Navbar, Header, Content, Footer } from "./components";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Header />
+      <Content />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
