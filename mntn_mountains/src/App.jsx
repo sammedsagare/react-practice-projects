a3a23608924c09b8f189bf0e77f97235bf75bcc6
Sammed Sagare <76863715+sammedsagare@users.noreply.github.com>
2024-01-25 16:47:17 +0530

    navbar + header without images. Incomplete slider

--- mntn_mountains/src/App.jsx
+++ mntn_mountains/src/App.jsx
@@ -1,15 +1,82 @@
-import { useState } from "react";
-import { Navbar, Header, Content, Footer } from "./components";
+// JSX (App.js)
 
-function App() {
+import React from "react";
+import account from "./assets/account.png";
+import instagram from "./assets/instagram.png";
+import twitter from "./assets/twitter.png";
+import arrow_down from "./assets/arrow-down.png";
+import slider from "./assets/slider.png";
+import one from "./assets/01.png";
+
+const App = () => {
   return (
-    <>
-      <Navbar />
-      <Header />
-      <Content />
-      <Footer />
-    </>
+    <div>
+      <nav>
+        <div id="logo">MNTN</div>
+        <div id="center-links">
+          <a id="link1" href="#">
+            Equipment
+          </a>
+          <a id="link2" href="#">
+            About Us
+          </a>
+          <a id="link3" href="#">
+            Blog
+          </a>
+        </div>
+        <div id="right-links">
+          <a href="#">
+            <img src={account} alt="Account" />
+          </a>
+          <a id="quick-link" href="#">
+            Account
+          </a>
+        </div>
+      </nav>
+      <header>
+        <div id="social-media-container">
+          <p>Follow Us</p>
+
+          <a href="#">
+            <img src={instagram} alt="Instagram" />
+          </a>
+          <a href="#">
+            <img src={twitter} alt="Twitter" />
+          </a>
+        </div>
+        <div id="textual-info">
+          <div className="header-info">
+            <div className="rectangular-line"></div>
+            <h3>A HIKING GUIDE</h3>
+          </div>
+          <h1>
+            Be Prepared For The
+            <br />
+            Mountains And Beyond!
+          </h1>
+          <a href="#scroll-down-button">
+            <button className="btn-1">
+              scroll down <img src={arrow_down} alt="" />
+            </button>
+          </a>
+        </div>
+        <div id="slider">
+          <div className="slider-section">
+            <a href="#start">Start</a>
+          </div>
+          <div className="slider-section">
+            <a href="#01">01</a>
+          </div>
+          <div className="slider-section">
+            <a href="#02">02</a>
+          </div>
+          <div className="slider-section">
+            <a href="#03">03</a>
+          </div>
+        </div>
+      </header>
+    </div>
   );
-}
+};
 
 export default App;
--- mntn_mountains/src/components/index.js
+++
@@ -1,4 +0,0 @@
-export { default as Navbar } from "./Navbar/Navbar";
-export { default as Header } from "./Header/Header";
-export { default as Content } from "./Content/Content";
-export { default as Footer } from "./Footer/Footer";
