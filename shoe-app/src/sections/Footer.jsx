6d91f9b15368f016a9d6b3e5765410c8efa6cd3d
Sammed Sagare <76863715+sammedsagare@users.noreply.github.com>
2024-01-29 17:45:50 +0530

    typo fix

--- shoe-app/src/sections/Footer.jsx
+++ shoe-app/src/sections/Footer.jsx
@@ -17,8 +17,8 @@ const Footer = () => {
             />
           </a>
           <p className="mt-6 text-base leading-7 font-montserrat  sm:max-w-sm">
-            Get shoes ready for the new term at your nearest Nike store. Find
-            Your perfect Size In Store. Get Rewards
+            Get shoes ready for the new term at your nearest easysole store.
+            Find Your perfect Size In Store. Get Rewards
           </p>
           <div className="flex items-center gap-5 mt-8">
             {socialMedia.map((icon) => (
