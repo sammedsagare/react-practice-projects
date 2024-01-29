04993372b6cc1daf6bff02a3699cd5446c867108
Sammed Sagare <76863715+sammedsagare@users.noreply.github.com>
2024-01-29 11:11:18 +0530

    project setup

---
+++ shoe-app/src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <>
+      <div className="text-center text-2xl font-mono font-extrabold text-white">
+        Shoe App
+      </div>
+    </>
+  );
+}
